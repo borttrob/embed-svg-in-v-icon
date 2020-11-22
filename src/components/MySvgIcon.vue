12061b264d858c9c38f8f366d51a7108a7d4bbfc
bort <b.terentiev@gmail.com>
2020-11-22 20:54:52 +0100

    Initial commit

---
+++ src/components/MySvgIcon.vue
@@ -0,0 +1,14 @@
+<template>
+  <v-img>
+    <svg viewBox="0 0 200 200">
+      <circle
+        cx="100"
+        cy="100"
+        r="99"
+        stroke="black"
+        stroke-width="1"
+        fill="red"
+      />
+    </svg>
+  </v-img>
+</template>
--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,6 +1,14 @@
 import Vue from "vue";
 import Vuetify from "vuetify/lib";
+import MySvgIcon from "@/components/MySvgIcon";
 
 Vue.use(Vuetify);
 
-export default new Vuetify({});
+export default new Vuetify({
+  icons: {
+    iconfont: "mdi", // default - only for display purposes
+    values: {
+      mySvgIcon: { component: MySvgIcon }
+    }
+  }
+});
